0ca158a7a175349dc1dc4522e07485ad2a7e4790
deepthi5286 <deepthi5286@gmail.com>
2024-05-23 14:53:25 -0400

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/bash 
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+containerid='docker ps | awk -F " " '{print $1}'' #awk command-to get string from long line. -F demiliter by space and get the first string
+docker rm -f $containerid
